6f2425a5b7abd2914f0131ad858c0b961c40bad7
이유림 <lyl5152@naver.com>
2024-12-16 17:36:22 +0900

    second

---
+++ src/components/common/box/StrengthContentsBox.jsx
@@ -0,0 +1,61 @@
+import StrengthContents from "../shape/StrengthContents";
+
+const StrengthContentsBox = () => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(2,1fr)",
+        gap: "50px",
+      }}
+    >
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+      <StrengthContents
+        contents={
+          "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
+        }
+      />
+    </div>
+  );
+};
+
+export default StrengthContentsBox;
--- src/components/layout/Strength.jsx
+++ src/components/layout/Strength.jsx
@@ -1,5 +1,5 @@
+import StrengthContentsBox from "../common/box/StrengthContentsBox";
 import StrenthToggleBox from "../common/box/StrenthToggleBox";
-import StrengthContents from "../common/shape/StrengthContents";
 
 const Strength = () => {
   return (
@@ -16,47 +16,7 @@ const Strength = () => {
     >
       <h1 style={{ color: "white", fontSize: "50px" }}>Strength.</h1>
       <StrenthToggleBox />
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(2,1fr)",
-          gap: "30px",
-        }}
-      >
-        <StrengthContents
-          contents={
-            "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
-          }
-        />
-        <StrengthContents
-          contents={
-            "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
-          }
-        />
-        <StrengthContents
-          contents={
-            "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
-          }
-        />
-        <StrengthContents
-          contents={
-            "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
-          }
-        />
-        <StrengthContents
-          contents={
-            "redux, redux saga, recoil 등의 상태관리 라이브러리 사용 경험이 있습니다. next.js를 활용한 SSR 개발 경험이 있습니다."
-          }
-        />
-        <StrengthContents contents={"능력에 대해 적기"} />
-        <StrengthContents contents={"능력에 대해 적기"} />
-        <StrengthContents contents={"능력에 대해 적기"} />
-        <StrengthContents contents={"능력에 대해 적기"} />
-        <StrengthContents contents={"능력에 대해 적기"} />
-        <StrengthContents contents={"능력에 대해 적기"} />
-        <StrengthContents contents={"능력에 대해 적기"} />
-        <StrengthContents contents={"능력에 대해 적기"} />
-      </div>
+      <StrengthContentsBox />
     </article>
   );
 };
